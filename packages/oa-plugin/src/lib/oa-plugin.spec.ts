e1f2a2ab7e470130d1bd679582cc36aabe590b10
Chris Trzesniewski <k.trzesniewski@gmail.com>
2022-11-13 21:28:22 +0100

    First Stream results

--- packages/oa-plugin/src/lib/oa-plugin.spec.ts
+++ packages/oa-plugin/src/lib/oa-plugin.spec.ts
@@ -1,7 +1,160 @@
-import { oaPlugin } from './oa-plugin';
+import {
+  getRestrictions,
+  getTags,
+  OAPluginTag,
+  Restriction,
+} from '@over-architecture-plugin/oa-plugin';
 
-describe('oaPlugin', () => {
-  it('should work', () => {
-    expect(oaPlugin()).toEqual('oa-plugin');
+describe('getTags', () => {
+  it('should work with example 1', () => {
+    const testLibraryPath = 'libs/techA/scopeX/my-library';
+
+    expect(getTags(testLibraryPath)).toEqual([
+      'oa-plugin-level-0:techA',
+      'oa-plugin-level-1:scopeX',
+    ]);
+  });
+
+  it('should work with example 2', () => {
+    const testLibraryPath = 'libs/techB/scopeShared/my-library';
+
+    expect(getTags(testLibraryPath)).toEqual([
+      'oa-plugin-level-0:techB',
+      'oa-plugin-level-1:scopeShared',
+    ]);
+  });
+});
+
+describe('getRestrictions', () => {
+  it('should return restrictions', () => {
+    const given = Array<OAPluginTag>(
+      'oa-plugin-level-0:techA',
+      'oa-plugin-level-1:scopeX'
+    );
+
+    const expected = Array<Restriction>(
+      {
+        sourceTag: 'oa-plugin-level-0:techA',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-0:techA',
+          'oa-plugin-level-0:shared',
+        ],
+      },
+      {
+        sourceTag: 'oa-plugin-level-1:scopeX',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-1:scopeX',
+          'oa-plugin-level-1:shared',
+          'oa-plugin-level-0:shared',
+        ],
+      }
+    );
+
+    expect(getRestrictions(given)).toEqual(expected);
+  });
+
+  it('should return restrictions - 2', () => {
+    const given = Array<OAPluginTag>(
+      'oa-plugin-level-0:techB',
+      'oa-plugin-level-1:scopeY'
+    );
+
+    const expected = Array<Restriction>(
+      {
+        sourceTag: 'oa-plugin-level-0:techB',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-0:techB',
+          'oa-plugin-level-0:shared',
+        ],
+      },
+      {
+        sourceTag: 'oa-plugin-level-1:scopeY',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-1:scopeY',
+          'oa-plugin-level-1:shared',
+          'oa-plugin-level-0:shared',
+        ],
+      }
+    );
+
+    expect(getRestrictions(given)).toEqual(expected);
+  });
+
+  it('should return restrictions - with shared libs', () => {
+    const given = Array<OAPluginTag>(
+      'oa-plugin-level-0:techB',
+      'oa-plugin-level-1:scopeY',
+      'oa-plugin-level-1:shared'
+    );
+
+    const expected = Array<Restriction>(
+      {
+        sourceTag: 'oa-plugin-level-0:techB',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-0:techB',
+          'oa-plugin-level-0:shared',
+        ],
+      },
+      {
+        sourceTag: 'oa-plugin-level-1:scopeY',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-1:scopeY',
+          'oa-plugin-level-1:shared',
+          'oa-plugin-level-0:shared',
+        ],
+      },
+      {
+        sourceTag: 'oa-plugin-level-1:shared',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-1:shared',
+          'oa-plugin-level-0:shared',
+        ],
+      }
+    );
+
+    expect(getRestrictions(given)).toEqual(expected);
+  });
+
+  it('should return restrictions - with shared libs 2', () => {
+    const given = Array<OAPluginTag>(
+      'oa-plugin-level-0:techA',
+      'oa-plugin-level-0:techB',
+      'oa-plugin-level-1:scopeY',
+      'oa-plugin-level-1:shared'
+    );
+
+    const expected = Array<Restriction>(
+      {
+        sourceTag: 'oa-plugin-level-0:techA',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-0:techA',
+          'oa-plugin-level-0:shared',
+        ],
+      },
+      {
+        sourceTag: 'oa-plugin-level-0:techB',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-0:techB',
+          'oa-plugin-level-0:shared',
+        ],
+      },
+      {
+        sourceTag: 'oa-plugin-level-1:scopeY',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-1:scopeY',
+          'oa-plugin-level-1:shared',
+          'oa-plugin-level-0:shared',
+        ],
+      },
+      {
+        sourceTag: 'oa-plugin-level-1:shared',
+        onlyDependOnLibsWithTags: [
+          'oa-plugin-level-1:shared',
+          'oa-plugin-level-0:shared',
+        ],
+      }
+    );
+
+    expect(getRestrictions(given)).toEqual(expected);
   });
 });
--- packages/oa-plugin/src/lib/oa-plugin.ts
+++ packages/oa-plugin/src/lib/oa-plugin.ts
@@ -1,3 +1,70 @@
-export function oaPlugin(): string {
-  return 'oa-plugin';
+import * as path from 'path';
+
+/*
+ * techA
+ *     scopeX
+ *        * libX_A
+ *        * libX_B
+ *     scopeY
+ *        * libY_A
+ *        * libY_B
+ *     shared
+ *        * libShared_A
+ *        * libShared_B
+ * techB
+ *     scopeX
+ *        * libX_A
+ *        * libX_B
+ *     scopeY
+ *        * libY_A
+ *        * libY_B
+ *     shared
+ *        * libShared_A
+ *        * libShared_B
+ * shared
+ * */
+
+export function getTags(libPath: string): string[] {
+  return libPath
+    .split(path.sep)
+    .slice(1, -1)
+    .map((name, index) => `oa-plugin-level-${index}:${name}`);
+}
+
+type OAPluginCategory = `oa-plugin-level-${number}`;
+export type OAPluginTag<TagName extends string = string> =
+  `${OAPluginCategory}:${TagName}`;
+
+export type Restriction = {
+  sourceTag: OAPluginTag;
+  onlyDependOnLibsWithTags: OAPluginTag[];
+};
+
+const range = (n: number) => {
+  const r = [];
+  for (let i = n; i >= 0; i--) {
+    r.push(i);
+  }
+  return r;
+};
+
+export function getRestrictions<TagName extends string = string>(
+  tags: OAPluginTag<TagName>[]
+): Array<Restriction> {
+  return tags.map((tag) => {
+    const [category] = tag.split(':') as [OAPluginCategory, TagName];
+
+    const [_, levelString] = category.split('oa-plugin-level-');
+    const level = parseInt(levelString);
+
+    return {
+      sourceTag: tag,
+      onlyDependOnLibsWithTags: [
+        ...new Set([
+          tag,
+          ...range(level).map((l) => `oa-plugin-level-${l}:shared` as const),
+        ]),
+      ],
+    };
+  });
 }
